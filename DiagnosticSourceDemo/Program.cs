2412658d2f620a6c9352b989d00c0ff22b123e39
Jerry <jerryc@miniasp.com>
2020-12-15 17:41:31 +0800

    加入 DiagnosticSourceDemo 專案。

---
+++ DiagnosticSourceDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiagnosticSourceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
