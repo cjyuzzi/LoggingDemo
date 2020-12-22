55701d67d99867513f0f86e9b5882f82bdbe2b87
Jerry <jerryc@miniasp.com>
2020-12-22 10:10:15 +0800

    加入 NLog Demo 專案。

---
+++ NLogDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] _)
+        {
+            try
+            {
+                Logger.Info("Hello world");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Goodbye cruel world");
+            }
+        }
+    }
+}
